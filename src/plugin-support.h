f86498f3336f8dff5603a8c5d8bfbe28ec70519d
Tomáš Diblík <dibla.tomas@post.cz>
2024-02-23 13:08:27 +0100

    [Init] Setting things up

--- src/plugin-support.h
+++ src/plugin-support.h
@@ -1,21 +1,3 @@
-/*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program. If not, see <https://www.gnu.org/licenses/>
-*/
-
 #pragma once
 
 #ifdef __cplusplus
@@ -27,11 +9,11 @@ extern "C" {
 #include <stdarg.h>
 #include <string.h>
 
-extern const char *PLUGIN_NAME;
-extern const char *PLUGIN_VERSION;
+    extern const char* PLUGIN_NAME;
+    extern const char* PLUGIN_VERSION;
 
-void obs_log(int log_level, const char *format, ...);
-extern void blogva(int log_level, const char *format, va_list args);
+    void obs_log(int log_level, const char* format, ...);
+    extern void blogva(int log_level, const char* format, va_list args);
 
 #ifdef __cplusplus
 }
---
+++ temp-build-arch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf build_x86_64
+cmake --preset linux-x86_64
+cd build_x86_64
+ninja
+sudo cp obs-novby-protector.so /usr/lib/obs-plugins/
